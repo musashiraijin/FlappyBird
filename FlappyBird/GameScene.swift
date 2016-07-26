ef934b613c16d7d537ffbe70dbf34bd2e57d46fc
Musashi Raijin <Raijin@raijin.local>
2016-07-26 15:02:38 +0900

    Lesson7 5.1 GameSceneクラスを作成

---
+++ FlappyBird/GameScene.swift
@@ -0,0 +1,13 @@
+//
+//  GameScene.swift
+//  FlappyBird
+//
+//  Created by Musashi Raijin on 2016/07/26.
+//  Copyright © 2016年 masahiro.kitamura. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+
+}
