ff7ca9e7b1a0ffbe2c23185e8820cd54b7588ea4
Musashi Raijin <Raijin@raijin.local>
2016-07-26 15:34:58 +0900

    Lesson7 6.2 空をスクロールさせる 一応ビルドに成功する。しかし、groundとcloudのイメージがないと警告が出て、地面と雲がバツ印の画像で表示される。 ビルドは成功するが、コードに黄色い注意が出る。C styleがどうとか。

--- FlappyBird/GameScene.swift
+++ FlappyBird/GameScene.swift
@@ -22,6 +22,11 @@ class GameScene: SKScene {
         scrollNode = SKNode()
         addChild(scrollNode)
         
+        setupGround()
+        setupCloud()
+    }
+    
+    func setupGround() {
         // 地面の画像を読み込む
         let groundTexture = SKTexture(imageNamed: "ground")
         groundTexture.filteringMode = SKTextureFilteringMode.Nearest
@@ -39,7 +44,7 @@ class GameScene: SKScene {
         // 左にスクロール->元の位置->左にスクロールと無限に繰り替えるアクション
         let repeatScrollGround = SKAction.repeatActionForever(SKAction.sequence([moveGround, resetGround]))
         
-        // groundのスプライトを配置する
+        // スプライトを配置する
         for var i:CGFloat = 0; i < needNumber; i += 1 {
             let sprite = SKSpriteNode(texture: groundTexture)
             
@@ -51,6 +56,41 @@ class GameScene: SKScene {
             
             // スプライトを追加する
             scrollNode.addChild(sprite)
+        }
+    }
+    
+    func setupCloud() {
+        // 雲の画像を読み込む
+        let cloudTexture = SKTexture(imageNamed: "cloud")
+        cloudTexture.filteringMode = SKTextureFilteringMode.Nearest
+        
+        // 必要な枚数を計算
+        let needCloudNumber = 2.0 + (frame.size.width / cloudTexture.size().width)
+        
+        // スクロールするアクションを作成
+        // 左方向に画像一枚分スクロールさせるアクション
+        let moveCloud = SKAction.moveByX(-cloudTexture.size().width , y: 0, duration: 20.0)
+        
+        // 元の位置に戻すアクション
+        let resetCloud = SKAction.moveByX(cloudTexture.size().width, y: 0, duration: 0.0)
+        
+        // 左にスクロール->元の位置->左にスクロールと無限に繰り替えるアクション
+        let repeatScrollCloud = SKAction.repeatActionForever(SKAction.sequence([moveCloud, resetCloud]))
+        
+        // スプライトを配置する
+        for var i:CGFloat = 0; i < needCloudNumber; i += 1 {
+            let sprite = SKSpriteNode(texture: cloudTexture)
+            sprite.zPosition = -100 // 一番後ろになるようにする
+            
+            // スプライトの表示する位置を指定する
+            sprite.position = CGPoint(x: i * sprite.size.width, y: size.height - cloudTexture.size().height / 2)
+            
+            // スプライトにアニメーションを設定する
+            sprite.runAction(repeatScrollCloud)
+            
+            // スプライトを追加する
+            scrollNode.addChild(sprite)
+        }
     }
 
 }
